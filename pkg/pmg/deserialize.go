1ae6aee6a540dea6d42e5453260308841a4e39f5
Liam Gensel <liamgens@gmail.com>
2018-09-08 20:28:09 -0400

    added Rectangles for my boys

--- pkg/pmg/deserialize.go
+++ pkg/pmg/deserialize.go
@@ -3,6 +3,7 @@ package pmg
 import (
 	"encoding/json"
 	"fmt"
+	"image"
 	"io/ioutil"
 	"os"
 )
@@ -22,28 +23,38 @@ type George struct {
 	Name        string `json:"name"`
 	Image       string `json:"image"`
 	Accessories []Bling
+	Pieces      GeorgePieces `json:"pieces"`
+	Rectals     map[string]image.Rectangle
+}
+
+// GeorgePieces which are real cute and adorable
+type GeorgePieces struct {
+	Head  [4]int `json:"head"`
+	Face  [4]int `json:"face"`
+	Neck  [4]int `json:"neck"`
+	Torso [4]int `json:"torso"`
+	Legs  [4]int `json:"legs"`
+	Hands [4]int `json:"hands"`
+	Feet  [4]int `json:"feet"`
 }
 
 //Bling struct for bling that George can wear
 type Bling struct {
-	Noun     string   `json:"noun"`
-	Adj      string   `json:"adj"`
-	Location Location `json:"location"`
-	Image    string   `json:"image"`
+	Noun     string `json:"noun"`
+	Adj      string `json:"adj"`
+	Location string `json:"location"`
+	Image    string `json:"image"`
 }
-
-// Location for the bling
-type Location string
 
 // Location enum for where the bling should be placed on George
 const (
-	HEAD Location = "HEAD"
-	FACE Location = "FACE"
-	NECK Location = "NECK"
-	TORSO Location = "TORSO"
-	LEGS Location = "LEGS"
-	HANDS Location = "HANDS"
-	FEET Location = "FEET"
+	HEAD  string = "HEAD"
+	FACE  string = "FACE"
+	NECK  string = "NECK"
+	TORSO string = "TORSO"
+	LEGS  string = "LEGS"
+	HANDS string = "HANDS"
+	FEET  string = "FEET"
 )
 
 // FetchGeorgeBlingData Parses JSON files and returns slices of the George structs and Bling structs
@@ -71,5 +82,26 @@ func FetchGeorgeBlingData(georgesPath string, blangPath string) ([]George, []Bli
 	json.Unmarshal(georgesBytes, &georges)
 	json.Unmarshal(blangBytes, &blang)
 
+	for i := 0; i < len(georges.Georges); i++ {
+		georges.Georges[i] = populateMap(georges.Georges[i])
+	}
+
 	return georges.Georges, blang.Blang
+}
+
+func listToRect(coord [4]int) image.Rectangle {
+	return image.Rect(coord[0], coord[1], coord[2], coord[3])
+}
+
+func populateMap(g George) George {
+	g.Rectals = make(map[string]image.Rectangle)
+	g.Rectals[HEAD] = listToRect(g.Pieces.Head)
+	g.Rectals[FACE] = listToRect(g.Pieces.Face)
+	g.Rectals[NECK] = listToRect(g.Pieces.Neck)
+	g.Rectals[TORSO] = listToRect(g.Pieces.Torso)
+	g.Rectals[LEGS] = listToRect(g.Pieces.Legs)
+	g.Rectals[HANDS] = listToRect(g.Pieces.Hands)
+	g.Rectals[FEET] = listToRect(g.Pieces.Feet)
+
+	return g
 }
