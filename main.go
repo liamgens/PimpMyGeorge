3aeeeee8d7d8560eaa3a3e6c5fe7f378280a52f0
Peter Gottesman <peter@petergottesman.com>
2018-09-08 20:00:49 -0700

    MAN WRITES 140 CRAPPY LINES OF CODE, MURDERED BY TEAMMATES

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/liamgens/Gim/pkg/pmg"
 )
 
 func main() {
-	g, b := pmg.FetchGeorgeBlingData("./pkg/pmg/georges.json", "./pkg/pmg/blang.json")
-	fmt.Println(b)
-
-	//Iterate over all Georges
-	for i := 0; i < len(g); i++ {
-		//Iterate over each georges pieces and print it out
-		fmt.Println(g[i].Rectals)
-	}
-
+	// Dance for me George
+	pmg.MainLoop()
 }
